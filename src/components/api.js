7d3389e10c6585010a63f88d95c5a25ad65f5cae
“nugo” <nugzari.rostiashvili.1@btu.edu.ge>
2023-10-23 16:43:36 +0400

    feat: search cities using api

---
+++ src/components/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f8060f87a2msh61414548a38f72ep1f8674jsn583c78af14fd",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
